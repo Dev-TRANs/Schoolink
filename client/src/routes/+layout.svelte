0c1cbf4dd1c2e3f8b4ec3fd2cbbde1eb6ff9c3a7
forelps <dev.forelps@gmail.com>
2025-04-02 05:05:44 +0900

    feat: フロント復元

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -4,4 +4,44 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header class="sm:hidden py-4 fixed top-0 w-full shadow-md z-10 text-center backdrop-blur-xl bg-white/50">
+    <p class="text-3xl font-2xl font-bold font-['Allerta_Stencil']">Schoolink</p>
+</header>
+        
+<div class="grid grid-cols-1 sm:grid-cols-[16rem_auto] h-screen pt-16 sm:pt-0">
+    <div class="w-full bg-gray-200 hidden sm:block">
+    <p class="text-3xl font-2xl font-bold pl-5 mt-5 font-['Allerta_Stencil']">Schoolink</p>
+    <div class="text-lg mt-5 flex items-center mr-4 bg-gray-300 pl-5 py-2 rounded-r-xl">
+        <span class="material-symbols-outlined mr-1 text-blue-600">public</span>
+        プロジェクト
+    </div>
+    <div class="text-lg mt-1 flex items-center mr-4 pl-5 py-2 rounded-r-xl">
+    	<span class="material-symbols-outlined mr-1 text-blue-600">event</span>
+        イベント
+    </div>
+    <div class="text-lg mt-1 flex items-center mr-4 pl-5 py-2 rounded-r-xl">
+    	<span class="material-symbols-outlined mr-1 text-blue-600">account_circle</span>
+        マイページ
+    </div>
+    </div>
+    <div class="overflow-auto py-10 w-full">
+        {@render children()}
+    </div>
+</div>
+
+<footer class="sm:hidden bg-gray-100/80 backdrop-blur-xl fixed bottom-0 w-full shadow-lg">
+    <div class="grid grid-cols-3 w-full text-gray-600 font-bold">
+		<div class="text-xs flex flex-col items-center py-2 rounded-r-xl text-blue-600">
+			<span class="material-symbols-outlined text-blue-600 mb-1 !text-3xl">public</span>
+			プロジェクト
+		</div>
+		<div class="text-xs flex flex-col items-center py-2 rounded-r-xl">
+			<span class="material-symbols-outlined text-gray-600 mb-1 !text-3xl">event</span>
+			イベント
+		</div>
+		<div class="text-xs flex flex-col items-center py-2 rounded-r-xl">
+			<span class="material-symbols-outlined text-gray-600 mb-1 !text-3xl">account_circle</span>
+			マイページ
+		</div>
+	</div>
+</footer>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="mx-5">
+    <h1 class="text-4xl font-bold">プロジェクト</h1>
+    <input class="bg-gray-200 h-10 w-full mt-4 rounded-xl p-4" placeholder="プロジェクトを検索…" type="search"/>
+    <div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5">
+        <div>
+            <div class="text-sky-600 flex items-center justify-center text-6xl aspect-4/3 rounded-xl bg-gray-200">＋</div>
+            <p class="text-xl w-full text-center mt-2">新規作成</p>
+        </div>
+    </div>
+</div>
