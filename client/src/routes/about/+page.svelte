ebf399b82dd489c84d4885ccee0655749f5e5c7e
forelps <dev.forelps@gmail.com>
2025-07-15 22:43:32 +0900

    feat: about

---
+++ client/src/routes/about/+page.svelte
@@ -0,0 +1,29 @@
+<div class="w-4/5 mx-auto">
+    <p class="text-normal md:text-xl text-center">つながる学生のためのプラットフォーム</p>
+    <p class="text-6xl md:text-8xl font-bold font-AllertaStencil mt-4 text-center">Schoolink</p>
+    <p class="mt-5">Schoolinkは、<a href="https://www.seitokaishinko.org" class="underline">生徒会活動振興会</a>と<a href="https://www.stki.org" class="underline">生徒会情報機構（TRANs）</a>が共同開発する、生徒会団体、学生団体、生徒会のためのプラットフォームです。</p>
+    <div class="text-2xl mt-15 flex justify-center items-center text-center py-2 rounded-r-xl">
+        <span class="material-symbols-outlined mr-1 text-blue-600">public</span>
+        プロジェクト
+    </div>
+    <p class="mt-4">「<a class="underline" href="/projects">プロジェクト</a>」では、企画しているプロジェクトの情報をまとめたページを手軽に作成したり、ひと・ものを集めたりすることができます。また、気になるプロジェクトを見つけ、参加することができます。</p>
+    <div class="text-2xl mt-5 flex justify-center items-center text-center py-2 rounded-r-xl">
+        <span class="material-symbols-outlined mr-1 text-blue-600">quiz</span>
+        投票
+    </div>
+    <p class="mt-4">「<a class="underline" href="/polls">投票</a>」では、気になることをかんたんにアンケートできます。投票にはログインは必要ないので、リンクを共有するだけで広く意見を集めることができます。</p>
+    <div class="text-2xl mt-5 flex justify-center items-center text-center py-2 rounded-r-xl">
+    	<span class="material-symbols-outlined mr-1 text-blue-600">event</span>
+        イベント
+    </div>
+    <p class="mt-4">「<a class="underline" href="/events">イベント</a>」では、実施予定のイベントについてのページをかんたんに作成することができます。リンクを共有するだけでイベント情報を興味あるひとにイベントについてお知らせできます。</p>
+    <div class="w-full mt-15 flex flex-col items-center gap-5">
+      <p class="text-center text-3xl font-bold font-AllertaStencil">Project Schoolink</p>
+      <a href="https://seitokaishinko.org/" target="_blank" rel="noopener noreferrer">
+          <img src="/img/logo/seitokaishinko.png" alt="生徒会活動振興会" class="max-w-60" loading="lazy">
+      </a>
+      <a href="https://trans.stki.org/" target="_blank" rel="noopener noreferrer">
+          <img src="/img/logo/trans.svg" alt="TRANs - 生徒会情報機構" class="max-w-30" loading="lazy">
+      </a>
+    </div>
+</div>
