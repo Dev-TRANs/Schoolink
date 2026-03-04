5189646b251e86b6b45b27fa7d1315d5eb169bc0
ujex256 <105550500+ujex256@users.noreply.github.com>
2026-03-04 19:45:01 +0900

    fix: gitignoreされてないファイル群の削除

--- client/.svelte-kit/generated/client/nodes/11.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/polls/[pollId]/+page.svelte";
--- client/.svelte-kit/generated/client/nodes/13.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/polls/create/+page.svelte";
